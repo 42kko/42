8cd78ce8f25cf870331572587185eee346f8bc9e
kko <kko@student.42seoul.kr>
2022-11-04 16:53:23 +0900

    1

--- 03/minishell/test.c
+++ 03/minishell/test.c
@@ -6,27 +6,50 @@
 /*   By: kko <kko@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 10:48:18 by kko               #+#    #+#             */
-/*   Updated: 2022/10/31 11:07:18 by kko              ###   ########.fr       */
+/*   Updated: 2022/11/04 16:49:02 by kko              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <stdlib.h>
+#include <unistd.h>
 
-int	main()
+void	handler(int signum)
 {
-	char *a;
+	if (signum != SIGINT)
+		return ;
+	printf("cntl c\n");
+	if (rl_on_new_line() == -1)
+		exit(1);
+	rl_replace_line("", 1);
+	rl_redisplay();
+}
 
+int		main(void)
+{
+	char			*line;
+
+	signal(SIGINT, handler);
 	while (1)
 	{
-		a = readline("prompt :");
-		if (a)
-			printf("%s\n",a);
+		line = readline("prompt> ");
+		if (line)
+		{
+			if (strcmp(line, "exit") == 0)
+				exit(0);
+			if (ret)
+				printf("output> %s\n", line);
+			add_history(line);
+			free(line);
+			line = NULL;
+		}
 		else
-			break;
+			return (1);
 	}
-	free(a);
 	return (0);
 }
